6c50aee274ad9e65ce1295eb00709c84b719cea3
Justin-Guimond <trustinjusting@gmail.com>
2023-07-21 14:31:07 -0400

    Commit 1 - renamed folders

--- src/pages/NotesListPage/NotesListPage.jsx
+++ src/pages/NotesListPage/NotesListPage.jsx
@@ -1,14 +1,14 @@
 import { checkToken } from '../../utilities/users-service';
 
-export default function OrderHistoryPage() {
+export default function NotesListPage() {
   async function handleCheckToken() {
     const expDate = await checkToken();
-    console.log(expDate);
+    alert(expDate);
   }
   
   return (
     <>
-      <h1>OrderHistoryPage</h1>
+      <h1>Notes List</h1>
       <button onClick={handleCheckToken}>Check When My Login Expires</button>
     </>
   );
