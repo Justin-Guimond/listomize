6b768549612da4b456e82513f9858207720e98a4
josephthestar <joseph.rihan@domain.com>
2023-07-26 20:18:32 -0400

    added update but not working

--- controllers/api/entries.js
+++ controllers/api/entries.js
@@ -3,21 +3,35 @@ const Entry = require('../../models/entry');
 module.exports = {
     create,
     index,
+    edit,
     delete: deleteDay
-  };
-
+};
 
 async function index(req, res) {
-const entries = await Entry.find({});
-res.json(entries);
+    const entries = await Entry.find({});
+    res.json(entries);
 }
 
 async function create(req, res) {
-  const entries = await Entry.create(req.body);
-  res.json(entries);
+    const entry = await Entry.create(req.body);
+    res.json(entry);
 }
+ // i added this ************ 
+async function edit(req, res) {
+    try {
+        const updatedEntry = await Entry.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.json(updatedEntry);
+    } catch (error) {
+        res.status(500).json({ message: 'Error updating the entry' });
+    }
+}
+//i added this **********
 
 async function deleteDay(req, res) {
-    const entry = await Entry.findByIdAndRemove(req.params.id);
-    res.json({message: 'Entry deleted successfully'});
-};
+    try {
+        await Entry.findByIdAndRemove(req.params.id);
+        res.json({ message: 'Entry deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Error deleting the entry' });
+    }
+}
--- models/entry.js
+++ models/entry.js
@@ -1,12 +1,37 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
-const noteSchema = new Schema({
-    entry: {type: String, required: true},
-    user: {type: Schema.Types.ObjectId, ref:'User'},
-  }, {
-    timestamps: true
-  })
-  
-
-module.exports = mongoose.model('Entry', noteSchema);
+const entrySchema = new Schema({
+  details: {
+    AIModel: {
+      type: String,
+      required: true,
+    },
+    DevelopedBy: {
+      type: String,
+      required: true,
+    },
+    ReleasedDate: {
+      type: Date,
+      required: true,
+    },
+    Pros: {
+      type: String,
+      required: true,
+    },
+    Cons: {
+      type: String,
+      required: true,
+    },
+    entry: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+}, {
+  timestamps: true,
+});
+module.exports = mongoose.model('Entry', entrySchema);
